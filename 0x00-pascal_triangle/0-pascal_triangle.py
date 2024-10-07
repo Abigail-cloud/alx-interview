ef201dd3f3408b519971cc4da939da8d66dfeb14
Toluwalase Abigail <toluabby12@gmail.com>
2024-10-07 04:37:42 +0100

    Added a file for apscal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A script to  that returns a list of lists of integers representing the Pascal's triangle of n"""
+
+def pascal_triangle(n):
+  """
+    Generates Pascal's triangle of n rows.
+    
+    Args:
+        n (int): The number of rows for Pascal's triangle.
+        
+    Returns:
+        List[List[int]]: A list of lists representing the Pascal's triangle.
+    """
+  triangle = []
+ 
+  # return (trianlgle if n <= 0)
+  if n <= 0:
+    return triangle
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
